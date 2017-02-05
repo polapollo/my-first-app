88da2e9f99bf6533eac3dc8c5cf74d5b35b1c840
Jakub Pavel <pakuba@gmail.com>
2017-02-05 18:29:53 +0100

    Added CSS style

--- blog/models.py
+++ blog/models.py
@@ -15,4 +15,4 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return "( " + self.title + " ) = " + str(self.author)
+        return str(self.pk) + "( " + self.title + " ) = " + str(self.author)
